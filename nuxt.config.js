0c07a1febe7153c71a821f444d60d132b631461d
myungwoo <myungwoo.dev@gmail.com>
2022-06-29 01:27:23 +0900

    feat: 진짜 init

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,20 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
-
+  
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "nuxt-blog",
     htmlAttrs: {
-      lang: "en",
+      lang: "en"
     },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+      {charset: "utf-8"},
+      {name: "viewport", content: "width=device-width, initial-scale=1"},
+      {hid: "description", name: "description", content: ""},
+      {name: "format-detection", content: "telephone=no"},
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{rel: "icon", type: "image/x-icon", href: "/favicon.ico"}],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -67,8 +67,5 @@ export default {
         autoprefixer: {},
       },
     },
-  },
-  router: {
-    base: '/blog'
-  },
+  }
 };
