1c9dba132b11676d38b5482bf0a397f2c12bb196
myungwoo <myungwoo.dev@gmail.com>
2022-07-07 23:23:16 +0900

    feat: firebase plugin import 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   css: ['@/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '@/plugins/firebase', ssr: false }],
+  plugins: [{ src: '~/plugins/firebase', ssr: false }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
