0cbcdc9d041a77a6b9584f393f00811eece12a2c
myungwoo <myungwoo.dev@gmail.com>
2022-07-07 23:22:32 +0900

    feat: 사이트 소유주 정보 수정

--- data/sitemetainfo.js
+++ data/sitemetainfo.js
@@ -1,28 +1,28 @@
 const siteMetaInfo = {
-  title: "Md Solaiaman | Software Developer",
-  author: "Md Solaiman",
-  author_image: "/author.png",
-  position: "Full Stack Developer",
-  location: "Dhaka, Bangladesh",
-  headerTitle: "Md Rathik",
-  slogan: "Let's start coding",
+  title: '명우의 잡동사니 블로그 ',
+  author: '고명우',
+  author_image: '/author.png',
+  position: 'Full Stack(을 지향하는) 개발자',
+  location: '경기도 수원',
+  headerTitle: 'Md Rathik',
+  slogan: '나 자신도 믿지말자',
   description:
-    "A tech enthusiast & self taught learner. Love to talk the way of tech.",
-  language: "en-us",
-  theme: "system", // system, dark or light
-  siteUrl: "https://rathik.dev",
-  siteRepo: "#",
-  siteLogo: "../assets/author.png",
-  image: "~/assets/author.png",
-  socialBanner: "/static/images/twitter-card.png",
-  email: "hello@rathik.dev",
-  github: "https://github.com/mdrathik",
-  githubUser: "mdrathik",
-  twitter: "#",
-  twitter_user: "MDRT83716131",
-  stackoverflow: "#",
-  youtube: "#",
-  linkedin: "#",
-};
+    'A tech enthusiast & self taught learner. Love to talk the way of tech.',
+  language: 'ko-kr',
+  theme: 'dark', // system, dark or light
+  siteUrl: 'https://blog.myungwoo.dev',
+  siteRepo: '#',
+  siteLogo: '../assets/author.png',
+  image: '~/assets/author.png',
+  socialBanner: '/static/images/twitter-card.png',
+  email: 'ko@myungwoo.dev',
+  github: 'https://github.com/mengkko',
+  githubUser: 'mengkko',
+  twitter: '#',
+  twitter_user: '#',
+  stackoverflow: '#',
+  youtube: '#',
+  linkedin: '#',
+}
 
-export default siteMetaInfo;
+export default siteMetaInfo
